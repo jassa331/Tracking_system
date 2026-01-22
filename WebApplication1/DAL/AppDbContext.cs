1c8819ce8c3007469a59bb06ae3da44cec174086
JASSADHAMMI <jassadhammi@gmail.com>
2026-01-22 16:58:23 +0530

    Add comment in AppDbContext class

--- WebApplication1/DAL/AppDbContext.cs
+++ WebApplication1/DAL/AppDbContext.cs
@@ -6,7 +6,7 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
-
+//jassa dhammi
     public DbSet<PatientAddress> PatientAddresses { get; set; }
     public DbSet<ApiLog>ApiLogs { get; set; }
 
